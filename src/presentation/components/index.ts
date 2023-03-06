6725b56468687c23b696fccf4f92cc6e982ea26c
rickutino <rickutino@gmail.com>
2023-03-06 20:31:09 +0900

    feat: add app router

--- src/presentation/components/index.ts
+++ src/presentation/components/index.ts
@@ -4,3 +4,4 @@ export { default as Input } from './input/input'
 export { default as LoginHeader } from './login-header/login-header' // You can use this component when you need it on Login or SignIn pages.
 export { default as Logo } from './logo/logo'
 export { default as Spinner } from './spinner/spinner'
+export { default as Router } from './router/router'
