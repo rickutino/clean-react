09548f5109bb91a246d6939f7263756834fae9c5
rickutino <rickutino@gmail.com>
2023-03-08 12:32:58 +0900

    test: ensure Login start with inputs with required state

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -7,8 +7,14 @@ describe('Login Component', () => {
     const { getByTestId } = render(<Login />)
     const errorWrap = getByTestId('error-wrap')
     const buttonSibmit = getByTestId('submit') as HTMLButtonElement
+    const emailStatus = getByTestId('email-status')
+    const passwordStatus = getByTestId('password-status')
 
     expect(errorWrap.childElementCount).toBe(0)
     expect(buttonSibmit.disabled).toBeTruthy()
+    expect(emailStatus.title).toBe('Required field')
+    expect(emailStatus.textContent).toBe('🔴')
+    expect(passwordStatus.title).toBe('Required field')
+    expect(passwordStatus.textContent).toBe('🔴')
   })
 })
